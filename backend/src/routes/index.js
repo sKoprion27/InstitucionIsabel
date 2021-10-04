ce2e6b3fff8da49e4248a30d4239df11b82cce29
Daniel Cu Sanchez <danielcu.sanchez@gmail.com>
2021-10-04 14:16:37 -0500

    Not found router

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,7 @@ import authRouter from './auth.routes'
 import donorsRouter from './donors.routes'
 import cfdisRouter from './cfdis.routes'
 import statesRouter from './states.routes'
+import notFoundRouter from './notFound.routes'
 // import { verifyToken } from '../lib/auth'
 
 export const initRoutes = (app) => {
@@ -14,4 +15,5 @@ export const initRoutes = (app) => {
   app.use('/notes', notesRouter)
   app.use('/cfdis', cfdisRouter)
   app.use('/states', statesRouter)
+  app.use('/*', notFoundRouter)
 }
