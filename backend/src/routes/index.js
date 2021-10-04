7883e14199fdd4be66ae8cfd4dee3c1546cf13d0
Daniel Cu Sanchez <danielcu.sanchez@gmail.com>
2021-10-03 21:05:52 -0500

    disable token

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,11 @@
 import usersRouter from './users.routes'
 import notesRouter from './notes.routes'
 import authRouter from './auth.routes'
-import { verifyToken } from '../lib/auth'
+// import { verifyToken } from '../lib/auth'
 
 export const initRoutes = (app) => {
   app.use('/auth', authRouter)
-  app.use(verifyToken)
+  // app.use(verifyToken) si se activa pide token
   app.use('/users', usersRouter)
   app.use('/donations', usersRouter)
   app.use('/donors', usersRouter)
