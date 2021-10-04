a2c6141f9a543dd314f391be4a9014cddb1d7d4f
LauRivero150920 <A01274144@itesm.mx>
2021-10-04 00:18:27 -0500

    CRUD donadores

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,7 @@
 import usersRouter from './users.routes'
 import notesRouter from './notes.routes'
 import authRouter from './auth.routes'
+import donorsRouter from './donors.routes'
 // import { verifyToken } from '../lib/auth'
 
 export const initRoutes = (app) => {
@@ -8,6 +9,6 @@ export const initRoutes = (app) => {
   // app.use(verifyToken) si se activa pide token
   app.use('/users', usersRouter)
   app.use('/donations', usersRouter)
-  app.use('/donors', usersRouter)
+  app.use('/donors', donorsRouter)
   app.use('/notes', notesRouter)
 }
