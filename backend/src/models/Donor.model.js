2de52297cde6dc9f76f6d297c380736a8a07822f
LauRivero150920 <A01274144@itesm.mx>
2021-10-04 23:05:31 -0500

    get, post donors

--- backend/src/models/Donor.model.js
+++ backend/src/models/Donor.model.js
@@ -8,11 +8,11 @@ export const Donor = {
     const { rows } = await db.query('SELECT * FROM donadores WHERE id = $1', [id])
     return rows[0]
   },
-  postOne: async (user) => {
-    const INSERTION = `INSERT INTO usuarios (id_cfdi, id_estado, nombre_contacto, telefono, razon_social, rfc, 
+  postOne: async (donor) => {
+    const INSERTION = `INSERT INTO donadores (id_cfdi, id_estado, nombre_contacto, telefono, razon_social, rfc, 
                         correo_electronico, codigo_postal, domicilio_fiscal, regimen_fiscal)
     VALUES ($1, $2, $3, $4, $5)`
-    const query = await db.query(INSERTION, [user.id_cfdi, user.id_estado, user.nombre_contacto, user.telefono, user.razon_social, user.rfc, user.correo_electronico, user.codigo_postal, user.domicilio_fiscal, user.regimen_fiscal])
+    const query = await db.query(INSERTION, [donor.id_cfdi, donor.id_estado, donor.nombre_contacto, donor.telefono, donor.razon_social, donor.rfc, donor.correo_electronico, donor.codigo_postal, donor.domicilio_fiscal, donor.regimen_fiscal])
     console.log('QUERY 😀', query)
     return query
   }
