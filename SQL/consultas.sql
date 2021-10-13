9c77c7e8ff08cb2854b7ea8914a22ea9fe9334b1
LauRivero150920 <A01274144@itesm.mx>
2021-10-13 12:25:24 -0500

    Consultas de notas, donadores, usuarios

--- SQL/consultas.sql
+++ SQL/consultas.sql
@@ -8,10 +8,10 @@ WHERE N.id_donacion = D.id AND N.id_usuario = U.id;
 -- Despliega telefono, razon_social, rfc, correo electronico, codigo postal, domicilio fiscal, regimen fiscal (True o false), Estado, Clave del CFDI, Descripcion del CFDI
 SELECT telefono, razon_social, rfc, correo_electronico, codigo_postal, domicilio_fiscal, regimen_fiscal, E.nombre as Estado, C.clave Clave_CFDI, C.descripcion Decripcion_CFDI
 FROM donadores D, cfdis C, estados E
-WHERE D.id_cfdi = C.id AND D.id_estado = E.id
+WHERE D.id_cfdi = C.id AND D.id_estado = E.id;
 
 -- Consultar Usuarios
 -- Despliega nombre, apellido, correo, rol
 SELECT nombre, apellido, correo_electronico, R.nombre_role Rol
 FROM usuarios U, roles R
-WHERE U.id_role = R.id
+WHERE U.id_role = R.id;
