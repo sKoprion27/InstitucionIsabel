be8cd191d5df252cc80a1a965bc619414607d798
LauRivero150920 <A01274144@itesm.mx>
2021-10-20 16:18:42 -0500

    Editar donacion

---
+++ frontend/src/Components/Donations/DonationEditForm/index.js
@@ -0,0 +1,83 @@
+import './style.scss'
+import { useEffect, useState } from 'react'
+import { getOneDonation, updateDonation } from './../../../helpers/donations.helpers'
+import { useParams } from 'react-router'
+export const DonationEditForm = () => {
+  const { id } = useParams()
+  const [donation, setDonation] = useState({
+    nombre_donacion: '',
+    monto: '',
+    metodo_pago: '',
+    tipo_donacion: '',
+    razon_social: '',
+    rfc: ''
+  })
+  const { nombre_donacion, monto, metodo_pago, tipo_donacion, razon_social, rfc } = donation
+
+  useEffect(() => {
+    const getDonation = async () => {
+      try {
+        const donation = await getOneDonation(id)
+        setDonation(donation)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getDonation()
+  }, [])
+
+  const handlerChange = (e) => {
+    setDonation(() => {
+      return {
+        ...donation
+      }
+    })
+  }
+  console.log(donation)
+
+  const handlerSubmit = async (e) => {
+    e.preventDefault()
+    console.log(donation, '🚀')
+    try {
+      const donationnew = { id, ...donation }
+      const response = await updateDonation(donationnew, id)
+      console.log(response, '😀')
+    } catch (error) {
+      console.log(error.response, '😆')
+    }
+  }
+  return (
+    <form className='donor__form' onSubmit={handlerSubmit}>
+      <div className='mb-3'>
+        <label className='form-label'>Nombre Donacion</label>
+        <input onChange={handlerChange} type='text' className='form-control' name='nombre_donacion' value={nombre_donacion} />
+      </div>
+      <div className='mb-3'>
+        <label className='form-label'>Monto</label>
+        <input onChange={handlerChange} type='text' className='form-control' name='monto' value={monto} />
+      </div>
+      <div className='mb-3'>
+        <label className='form-label'>Método Pago</label>
+        <input onChange={handlerChange} type='text' className='form-control' name='metodo_pago' value={metodo_pago} />
+      </div>
+      <div className='mb-3'>
+        <label className='form-label'>Tipo Donacion</label>
+        <input onChange={handlerChange} type='text' className='form-control' name='tipo_donacion' value={tipo_donacion} />
+      </div>
+      <div className='mb-3'>
+        <label className='form-label'>Razon Social</label>
+        <input onChange={handlerChange} type='text' className='form-control' name='razon_social' value={razon_social} />
+      </div>
+      <div className='mb-3'>
+        <label className='form-label'>RFC</label>
+        <input onChange={handlerChange} type='text' className='form-control' name='rfc' value={rfc} />
+      </div>
+      <div className='mb-3'>
+      </div>
+      <div className='user__btn__container'>
+        <button type='submit' className='btn btn-secondary btn-lg' >Actualizar donador</button>
+      </div>
+
+    </form>
+  )
+}
--- frontend/src/Pages/Donations/DonationEdit/index.js
+++ frontend/src/Pages/Donations/DonationEdit/index.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { getOneDonor } from '../../../helpers/donors.helpers'
+import { DonationEditForm } from './../../../Components/Donations/DonationEditForm/index'
+
 export const DonationEdit = () => {
+  const { id } = useParams()
+  const [donor, setDonor] = useState({})
+
+  useEffect(() => {
+    const getDonor = async () => {
+      try {
+        const donor = await getOneDonor(id)
+        setDonor(donor)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getDonor()
+  }, [id])
   return (
-    <div>
-      <h1>Donation Edit</h1>
+    <div className='row justify-content-center'>
+      <h1 className='text-center'>Editar Donación</h1>
+      <div className='col-6'>
+        <DonationEditForm {...donor} />
+      </div>
     </div>
   )
 }
