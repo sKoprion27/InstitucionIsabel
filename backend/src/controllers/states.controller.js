78ef78a0892e5d294dfbef502d07d051dd45d47d
LauRivero150920 <A01274144@itesm.mx>
2021-11-16 17:50:23 -0600

    fix cfdi, states controllers

--- backend/src/controllers/states.controller.js
+++ backend/src/controllers/states.controller.js
@@ -5,32 +5,61 @@ export const stateController = {
 
   // GET ALL
   getStates: async (req, res) => {
-    const [queryAnswer, status] = await State.getAll()
-    response(req, res, 'GET STATES', queryAnswer, status)
+    try {
+      const { rows } = await State.getAll()
+      response(req, res, 'GET STATES', rows, 200)
+    } catch (error) {
+      console.log(error)
+      response(req, res, 'ERROR GET STATES', null, 500)
+    }
   },
 
   // GET ONE
   getOneState: async (req, res) => {
     const { id } = req.params
-    const [queryAnswer, status] = await State.getOne(id)
-    response(req, res, 'GET ONE STATE', queryAnswer, status)
+    try {
+      const { rows, rowCount } = await State.getOne(id)
+      if (rowCount === 0) {
+        response(req, res, 'ERROR GET STATE', null, 500)
+        return
+      }
+      response(req, res, 'GET STATE', rows[0], 200)
+    } catch (error) {
+      console.log(error)
+      response(req, res, 'ERROR GET STATE', null, 500)
+    }
   },
   postOneState: async (req, res) => {
-    const state = { ...req.body }
-    const [queryAnswer, status] = await State.postOne(state)
-    response(req, res, 'POST ONE STATE', queryAnswer, status)
+    try {
+      const state = { ...req.body }
+      const { rowCount } = await State.postOne(state)
+      response(req, res, 'POST ONE STATE', rowCount, 201)
+    } catch (error) {
+      console.log(error)
+      response(req, res, 'ERROR POST ONE STATE', null, 500)
+    }
   },
   // UPDATE ONE
   updateOneState: async (req, res) => {
-    const state = req.body
-    const id = req.params.id
-    const [queryAnswer, status] = await State.putOne(state, id)
-    response(req, res, 'UPDATE ONE STATE', queryAnswer, status)
+    try {
+      const state = req.body
+      const id = req.params.id
+      const { rowCount } = await State.putOne(state, id)
+      response(req, res, 'UPDATE ONE STATE', rowCount, 201)
+    } catch (error) {
+      console.log(error)
+      response(req, res, 'ERROR UPDATE ONE STATE', null, 500)
+    }
   },
   // DELETE ONE
   deleteOneState: async (req, res) => {
-    const id = req.params.id
-    const [queryAnswer, status] = await State.deleteOne(id)
-    response(req, res, 'DELETE ONE STATE', queryAnswer, status)
+    try {
+      const id = req.params.id
+      const { rowCount } = await State.deleteOne(id)
+      response(req, res, 'DELETE ONE STATE', rowCount, 201)
+    } catch (error) {
+      console.log(error)
+      response(req, res, 'ERROR DELETE ONE STATE', null, 500)
+    }
   }
 }
--- frontend/src/Pages/Donors/DonorAdd/index.js
+++ frontend/src/Pages/Donors/DonorAdd/index.js
@@ -1,212 +1,375 @@
 /* eslint-disable no-useless-escape */
-import { getAllRoles, postOneUser } from '../../../helpers/users.helpers'
-import { useEffect, useState } from 'react'
-import { useForm, Controller } from 'react-hook-form'
+import {
+  useEffect,
+  useState
+} from 'react'
+import {
+  Navigate
+} from 'react-router-dom'
+import {
+  Controller,
+  useForm
+} from 'react-hook-form'
+
 import { NavPage } from '../../../Components/Dashboard/NavPage'
-import { MdVisibility, MdVisibilityOff } from 'react-icons/md'
+import { Card } from 'react-materialize'
 import Select from 'react-select'
 import makeAnimated from 'react-select/animated'
+
+import { postDonor } from '../../../helpers/donors.helpers'
+
 import './style.scss'
-import { Card } from 'react-materialize'
+import { convertToSelectOptions, filterSelectsOptiones, formatDateTable } from '../../../utils'
+import { getAllStates } from '../../../helpers/states.helpers'
+import { getAllCfdis } from './../../../helpers/cfdis.helpers'
 import { toastInit } from '../../../Components/Dashboard/AlertToast'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 export const DonorAdd = () => {
+  const animatedComponents = makeAnimated()
+  const [edit, setEdit] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [options, setOptions] = useState({
+    states: [],
+    cfdis: [],
+    regimen_fiscal: [{
+      label: 'PERSONA MORAL',
+      value: true
+    },
+    {
+      label: 'PERSONA FISICA',
+      value: false
+    }
+    ]
+  })
+
   const {
     register,
     handleSubmit,
-    reset, control,
+    reset,
+    control,
+    setValue,
     formState: { errors }
   } = useForm()
-  const [show, setShow] = useState(false)
-  const [roles, setRoles] = useState([{ value: 0, label: 'default' }])
 
-  const animatedComponents = makeAnimated()
+  const onChange = (e) => {
+    console.log(e.target.files[0])
+  }
 
   useEffect(() => {
-    const getRoles = async () => {
-      const roles = await getAllRoles()
-      const options = roles.map(r => {
-        return {
-          value: r.id,
-          label: r.nombre_role
-        }
-      })
-      setRoles(options)
-    }
-    getRoles()
-  }, [])
+    const getInfoSelects = async () => {
+      try {
+        const estados = await getAllStates()
+        const cfdis = await getAllCfdis()
 
-  const handlerShowPassword = () => {
-    setShow(!show)
-  }
+        setOptions({
+          ...options,
+          states: convertToSelectOptions(estados),
+          cfdis: convertToSelectOptions(cfdis)
+        })
+      } catch (error) {
+        console.log(error)
+        toastInit('Error al cargar la página', 'red lighten-2')
+        setEdit(null)
+      }
+    }
+    getInfoSelects()
+  }, [edit])
+
+  // Use Effect for selects info
 
   const handlerSubmit = async (data) => {
     try {
-      // console.log(data)
-      const roles = data.roles.map(role => {
-        return {
-          id: role.value,
-          nombre: role.label
-        }
-      })
+      setLoading(true)
+      setEdit(false)
       const dataPost = {
-        ...data,
-        roles
+        donor: {
+          telefono: data.telefono,
+          razon_social: data.razon_social,
+          rfc: data.rfc,
+          nombre: data.nombre,
+          correo_electronico: data.correo_electronico,
+          domicilio_fiscal: data.domicilio_fiscal,
+          codigo_postal: data.codigo_postal,
+          regimen_fiscal: data.regimen_fiscal.value,
+          id_estado: data.id_estado.value,
+          id_cfdi: data.id_cfdi.value
+        }
       }
-      await postOneUser(dataPost)
+      await postDonor(dataPost)
       toastInit('Elemento agregado')
-      reset({})
-      reset({ roles: '' })
+      setEdit(true)
+      setLoading(false)
+      resetForm()
     } catch (error) {
+      console.log(error)
       toastInit('Error al agregar', 'red lighten-2')
-      console.log(error, 'Crear usuario')
+      setEdit(true)
+      resetForm()
     }
+  }
+
+  const resetForm = () => {
+    reset({})
+    setValue('id_donador', 'value', { shouldDirty: true })
+    setValue('id_metodo_pago', 'value', { shouldDirty: true })
+    setValue('id_tipo_donacion', 'value', { shouldDirty: true })
+    setValue('categorias', 'value', { shouldDirty: true })
+    setValue('beneficiarios', 'value', { shouldDirty: true })
+  }
+
+  if (edit === null) {
+    return <Navigate to='/dashboard/NOTFOUND' />
   }
 
   return (
     <>
-      <NavPage title='Agregar donador' path='/dashboard/usuarios' />
+      <NavPage title='Agregar donador' path='/dashboard/donadores' />
       <Card className='hoverable'>
+        <h6 className='teal-text'>Información</h6>
+
         <form
-          className='user__form'
+          className='user__form '
           onSubmit={handleSubmit(handlerSubmit)}
         >
-
-          <div className='input-field'>
-            <label>Nombre</label>
+          <div>
+            <label>Telefono (Ej.1234567890)</label>
             <input
               onChange={register}
-              type='text'
-              name='nombre'
-              autoComplete={'off'}
+              type='tel'
+              autoComplete='off'
+              pattern='[0-9]{10}'
               {
-              ...register('nombre', {
-                required: true
+              ...register('telefono', {
+                required: {
+                  value: true,
+                  message: 'Este campo es requerido'
+                }
               })
               }
-            />
-            {errors.nombre?.type === 'required' &&
-              (<span className='red-text'>El nombre es requerido</span>)
+              disabled={!edit} />
+            {errors.telefono &&
+              (<span className='red-text'>
+                {
+                  errors.telefono.message
+                }
+              </span>)
             }
           </div>
-          <div className='input-field'>
-            <label>Apellidos</label>
+          <div>
+            <label>Razón Social</label>
             <input
               onChange={register}
               type='text'
-              className='input-field'
-              name='apellido'
-              autoComplete={'off'}
-              {...register('apellido', {
-                required: true
+              autoComplete='off'
+              {...register('razon_social', {
+                required: {
+                  value: true,
+                  message: 'Este campo es requerido'
+                }
               })}
-
+              disabled={!edit}
             />
-            {errors.apellido?.type === 'required' &&
-              (<span className='red-text'>El apellido es requerido</span>)
+            {errors.razon_social &&
+              (<span className='red-text'>
+                {
+                  errors.razon_social.message
+                }
+              </span>)
             }
           </div>
+          {/* <div>
+            <label>Facturado</label>
+            <input
+              onChange={register}
+              type='text'
+              autoComplete='off'
+              {...register('esta_facturado', {
+                required: {
+                  value: true,
+                  message: 'Este campo es requerido'
+                }
+              })}
+              disabled={!edit}
+            />
+            {errors.descripcion &&
+              (<span className='red-text'>
+                {
+                  errors.descripcion.message
+                }
+              </span>)
+            }
+          </div> */}
+          {/* SELECT DONDADOR */}
           <div className='input-select'>
-            <label>Selecciona los roles</label>
+            <label>Selecciona donador</label>
             <Controller
-              control={control}
-              rules={{ required: true }}
               defaultValue={false}
-              name='roles'
+              control={control}
+              name='id_donador'
               render={({ field }) => (
                 <Select
-                  placeholder='Roles de usuario'
+                  placeholder='Donador'
                   closeMenuOnSelect
                   components={animatedComponents}
-                  isMulti
-                  options={roles}
+                  options={options.donors}
                   {...field}
+                  isDisabled={!edit}
                 />
               )}
             />
-            {errors.roles?.type === 'required' &&
+            {errors.id_donador &&
               (<span
                 className='red-text'
               >
-                Selecciona al menos un role
+                {errors.id_donador.message}
               </span>)
             }
           </div>
-          <div className='input-field'>
-            <label>Correo electrónico</label>
-            <input
-              onChange={register}
-              type='text'
-              name='correo_electronico'
-              autoComplete={'off'}
-              {...register('correo_electronico', {
-                required: {
-                  value: true,
-                  message: 'El correo electronico es requerido'
-                },
-                pattern: {
-                  value: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                  message: 'Ingresa un correo válido'
-                }
-              })}
-
+          {/* SELECT MÉTODO DE PAGO */}
+          <div className='input-select'>
+            <label>Selecciona un método de pago</label>
+            <Controller
+              defaultValue={false}
+              control={control}
+              name='id_metodo_pago'
+              render={({ field }) => (
+                <Select
+                  placeholder='Método de pago'
+                  closeMenuOnSelect
+                  components={animatedComponents}
+                  options={options.paymentMethods}
+                  {...field}
+                  isDisabled={!edit}
+                />
+              )}
             />
-            {errors.correo_electronico &&
-              (<span className='red-text'>
-                {errors.correo_electronico.message}
+            {errors.id_metodo_pago &&
+              (<span
+                className='red-text'
+              >
+                {errors.id_metodo_pago.message}
               </span>)
             }
           </div>
-          <div className='input-field'>
-            <label>Contraseña</label>
-            <input
-              onChange={register}
-              type={show ? 'text' : 'password'}
-              name='password'
-              autoComplete='new-password'
-              {...register('password', {
-                required: {
-                  value: true,
-                  message: 'La contraseña es requerida'
-                },
-                minLength: {
-                  value: 8,
-                  message: 'La contraseña debe contener al menos 8 caracteres'
-                },
-                pattern: {
-                  value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/,
-                  message: 'La contraseña debe contener al menos una mayúscula y un numero'
-                }
-              })}
-
+          {/* SELECT TIPO DE DONACIÓN */}
+          <div className='input-select'>
+            <label>Selecciona que tipo de donativo</label>
+            <Controller
+              defaultValue={false}
+              control={control}
+              name='id_tipo_donacion'
+              render={({ field }) => (
+                <Select
+                  placeholder='Tipo donativo'
+                  closeMenuOnSelect
+                  components={animatedComponents}
+                  options={options.typesDonations}
+                  {...field}
+                  isDisabled={!edit}
+                />
+              )}
             />
-            {errors.password &&
-              (<span className='red-text'>
-                {errors.password.message}
+            {errors.id_tipo_donacion &&
+              (<span
+                className='red-text'
+              >
+                {errors.id_tipo_donacion.message}
               </span>)
             }
           </div>
+          {/* SELECT CATEGORIAS_DONACIONES */}
+          <div className='input-select'>
+            <label>Selecciona las categorias del donativo</label>
+            <Controller
+              control={control}
+              defaultValue={false}
+              name='categorias'
+              render={({ field }) => (
+                <Select
+                  placeholder='Categorias de donativo'
+                  closeMenuOnSelect
+                  components={animatedComponents}
+                  options={options.categories}
+                  isMulti
+                  {...field}
+                  isDisabled={!edit}
+                />
+              )}
+            />
+            {errors.categorias &&
+              (<span
+                className='red-text'
+              >
+                {errors.categorias.message}
+              </span>)
+            }
+          </div>
+          {/* SELECT DONACIONES_BENEFICIARIOS */}
+          <div className='input-select'>
+            <label>Selecciona los beneficiarios del donativo</label>
+            <Controller
+              control={control}
+              defaultValue={false}
+              name='beneficiarios'
+              render={({ field }) => (
+                <Select
+                  placeholder='Beneficiarios del donativo'
+                  closeMenuOnSelect
+                  isMulti
+                  components={animatedComponents}
+                  options={options.beneficiaries}
+                  {...field}
+                  isDisabled={!edit}
 
-          {
-            show
-              ? (<div className='show-password' onClick={handlerShowPassword}>
-                <MdVisibilityOff />
-                <span className='text-muted'>Ocultar contraseña</span>
-              </div>)
-              : (<div className='show-password' onClick={handlerShowPassword}>
-                <MdVisibility />
-                <span className='text-muted'>Mostrar contraseña</span>
-              </div>)
-          }
+                />
+              )}
+            />
+            {errors.beneficiarios &&
+              (<span
+                className='red-text'
+              >
+                {errors.beneficiarios.message}
+              </span>)
+            }
+          </div>
+          {/* AGREGAR DE FOTO */}
 
-          <div className='btn-submit'>
+          <div className='img-container-input'>
+            <div className='img-donacion'>
+              <p>Foto de la donación</p>
+              <input
+                id='foto_donacion'
+                type='file'
+                {...register('foto_donacion')}
+                onChange={onChange}
+              />
+            </div>
+            {errors.foto_donacion &&
+              (<span
+                className='red-text img-error'
+              >
+                {errors.foto_donacion.message}
+              </span>)
+            }
+          </div>
+          {/* BOTONES DE OPCIONES */}
+          <div className='user__btn__container'>
             <button
               type='submit'
-              className='btn btn-primary'
+              className='btn btn-success  '
+              disabled={loading || !edit}
             >
-              Agregar usuario
+              Agregar donación
             </button>
           </div>
+          {
+            loading && (
+              <div className='progress'>
+                <div className='indeterminate' />
+              </div>
+            )
+          }
         </form>
       </Card>
     </>
