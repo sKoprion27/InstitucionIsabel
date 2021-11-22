0d17b328e6cdde2acbf469cb495473b08cdc919e
Daniel Cu Sánchez <danielcu@alternet.com.mx>
2021-11-22 16:13:50 -0600

    Nombre del archivo beneficiarios

--- backend/src/lib/multer.js
+++ backend/src/lib/multer.js
@@ -19,7 +19,7 @@ const diskStorage = multer.diskStorage({
     cb(null, 'uploads/')
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname))
+    cb(null, `${file.originalname}_${Date.now()}` + path.extname(file.originalname))
   }
 })
 const multerUploadFile = multer({ storage: diskStorage })
